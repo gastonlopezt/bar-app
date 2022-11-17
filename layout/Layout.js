94870a191ed2f19f9c21b8372be5534095e945c7
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-11-17 12:31:14 -0300

    modal

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,26 @@
-import Head from 'next/head'
-import Sidebar from '../components/Sidebar'
+import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
+import ModalProducto from '../components/ModalProducto';
+import Modal from 'react-modal';
+import useQuiosco from '../hooks/useQuiosco';
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#__next");
+
 
 export default function Layout({children, pagina}) {
+
+    const { modal } = useQuiosco()
     return (
       <>
         <Head>
@@ -18,6 +37,14 @@ export default function Layout({children, pagina}) {
               </div>
             </main>
         </div>
+        {modal && (
+          <Modal
+            isOpen={modal}
+            style={customStyles}
+          >
+            <ModalProducto />
+          </Modal>
+        )}
       </>
     )
   }
